09105b545d4d33bd1783e3e6a75365402119d26b
haris <hariskumar.eth@gmail.com>
2025-04-14 14:01:57 +0530

    2-api

--- components/DietGenerator.jsx
+++ components/DietGenerator.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/navigation'
 
 const openai = new OpenAI({
   baseURL: 'https://openrouter.ai/api/v1',
-  apiKey: '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',
+  apiKey: process.env.SDK_KEY,
   dangerouslyAllowBrowser: 'true'// Make sure this is set in .env.local
 });
 
